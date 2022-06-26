e0d335e7e758c48bb484334c3c186256da1db70f
RosarioRu <mariarruval@gmail.com>
2022-06-25 21:54:07 -0700

    Wrap Car class in namespace called Models. Add 'using Models' to Program.cs file.

--- Car.cs
+++ Car.cs
@@ -1,21 +1,24 @@
 using System;
 
-public class Car
+namespace Models 
 {
-  public string MakeModel;
-  public int Price;
-  public int Miles;
-  //constructor for new instance of class named Car is below:
-  public Car (string makeModel, int price, int milage)
+  public class Car
   {
-    MakeModel = makeModel;
-    Price = price;
-    Miles = milage;
-  }
-  //below we add a method that will return a bool type for objects that are instances of Car:
-  public bool WorthBuying (int maxPrice) //WorthBuying() method takes one argument - maxPrice
-  {
-    return (Price <= maxPrice); //return true if the price is less than the maxPrice user enters, if not, return false
+    public string MakeModel;
+    public int Price;
+    public int Miles;
+    //constructor for new instance of class named Car is below:
+    public Car (string makeModel, int price, int milage)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = milage;
+    }
+    //below we add a method that will return a bool type for objects that are instances of Car:
+    public bool WorthBuying (int maxPrice) //WorthBuying() method takes one argument - maxPrice
+    {
+      return (Price <= maxPrice); //return true if the price is less than the maxPrice user enters, if not, return false
+    }
   }
 }
 
@@ -23,4 +26,4 @@ public class Car
 
 
 
-  
+    
