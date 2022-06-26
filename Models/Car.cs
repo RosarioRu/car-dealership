a11ca868b53d4642983bf5f74adb1794cec15fe9
RosarioRu <mariarruval@gmail.com>
2022-06-25 22:00:57 -0700

    Move Car.cs file inside of Models directory.

---
+++ Models/Car.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Dealership.Models 
+{
+  public class Car
+  {
+    public string MakeModel;
+    public int Price;
+    public int Miles;
+    //constructor for new instance of class named Car is below:
+    public Car (string makeModel, int price, int milage)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = milage;
+    }
+    //below we add a method that will return a bool type for objects that are instances of Car:
+    public bool WorthBuying (int maxPrice) //WorthBuying() method takes one argument - maxPrice
+    {
+      return (Price <= maxPrice); //return true if the price is less than the maxPrice user enters, if not, return false
+    }
+  }
+}
+
+
+
+
+
+    
