cd17ba106d0345f0570a50e39b298bd5d0f60bd0
RosarioRu <mariarruval@gmail.com>
2022-06-26 06:54:01 -0700

    Make fiels private and add a getter method that returns _makeModel

--- Models/Car.cs
+++ Models/Car.cs
@@ -4,16 +4,23 @@ namespace Dealership.Models
 {
   public class Car
   {
-    private string MakeModel;
-    private int Price;
-    private int Miles;
+    private string _makeModel;
+    private int _price;
+    private int _miles;
     //constructor for new instance of class named Car is below:
     public Car (string makeModel, int price, int milage)
     {
-      MakeModel = makeModel;
-      Price = price;
-      Miles = milage;
+      _makeModel = makeModel;
+      _price = price;
+      _miles = milage;
     }
+    
+    //below is a 'getter method':
+    public string GetMakeModel()
+    {
+      return _makeModel;
+    }
+
     //below we add a method that will return a bool type for objects that are instances of Car:
     public bool WorthBuying (int maxPrice) //WorthBuying() method takes one argument - maxPrice
     {
